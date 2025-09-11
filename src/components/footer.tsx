b0e8ac934e19999ab2c56aeeec0df6bb47b0d824
Andrin <65789180+codeofandrin@users.noreply.github.com>
2025-09-11 15:54:26 +0200

    Remove duration-150 because 150ms is default

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,9 +3,7 @@ import Link from "next/link"
 export default function Footer() {
   return (
     <footer className="mt-auto flex w-full justify-center">
-      <Link
-        href="/legal"
-        className="text-xs text-stone-700 transition-colors duration-150 hover:text-stone-500">
+      <Link href="/legal" className="text-xs text-stone-700 transition-colors hover:text-stone-500">
         Legal Notice
       </Link>
     </footer>
