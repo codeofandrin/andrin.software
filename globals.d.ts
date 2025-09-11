53d7b0eee1bc458c90e0448cf2c90e65c88483fc
Andrin <65789180+codeofandrin@users.noreply.github.com>
2025-09-11 16:55:21 +0200

    Fix lint

--- globals.d.ts
+++ globals.d.ts
@@ -3,6 +3,6 @@ declare module "*.svg" {
     const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>
     export default content
 }
-declare module "*.png";
-declare module "*.jpeg";
-declare module "*.jpg";
+declare module "*.png"
+declare module "*.jpeg"
+declare module "*.jpg"
