7767bd59c8eee2aec25d7cc999bedaed9f83b6da
Andrin Schaller <65789180+codeofandrin@users.noreply.github.com>
2025-09-11 16:56:45 +0200

    Fix lint workflow

--- globals.d.ts
+++ globals.d.ts
@@ -1,1 +1,8 @@
 declare module "*.md"
+declare module "*.svg" {
+    const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>
+    export default content
+}
+declare module "*.png"
+declare module "*.jpeg"
+declare module "*.jpg"
