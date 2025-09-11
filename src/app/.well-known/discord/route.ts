0bc92854194028ec66e592e99aa02d906bd89930
Andrin <65789180+codeofandrin@users.noreply.github.com>
2025-09-11 17:35:53 +0200

    Add discord domain verification

---
+++ src/app/.well-known/discord/route.ts
@@ -0,0 +1,7 @@
+export async function GET() {
+    return new Response("dh=254467be6472324fe951ae9f12dcddb334d574b6", {
+        headers: {
+            "Content-Type": "text/plain; charset=utf-8"
+        }
+    })
+}
