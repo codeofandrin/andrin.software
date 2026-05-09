89445fec7e8ebb2549bfec2322ce155b4250f502
Andrin Schaller <65789180+codeofandrin@users.noreply.github.com>
2026-05-09 17:49:24 +0200

    Implement footer for desktop

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -6,7 +6,7 @@ import SVGLogoSmall from "@/assets/svg/brand/logo_small.svg"
 
 function FooterNavigation() {
   return (
-    <div className="mt-8 grid gap-5">
+    <div className="mt-8 grid gap-5 md:mt-0 md:flex md:gap-15">
       {MENU_ITEMS.map(({ name, link }) => {
         return (
           <div key={`footer-item-${name}`}>
@@ -31,30 +31,45 @@ function FooterNavigation() {
 
 function FooterLegal() {
   return (
-    <div className="grid gap-5 border-t-1 border-neutral-300 py-5 text-neutral-400">
-      <div className="flex justify-center gap-10">
-        <Link href="/impressum">Impressum</Link>
-        <Link href="/datenschutz">Datenschutz</Link>
+    <>
+      {/* Desktop */}
+      <div className="mt-2 hidden justify-between border-t-[0.5px] border-neutral-300 pt-5 pb-5 text-neutral-400 md:flex">
+        <p className="text-center">Copyright © 2026 andrin.software. Alle Rechte vorbehalten.</p>
+        <div className="flex justify-center gap-10">
+          <Link href="/impressum">Impressum</Link>
+          <Link href="/datenschutz">Datenschutz</Link>
+        </div>
       </div>
-      <p className="text-center">Copyright © 2026 andrin.software. Alle Rechte vorbehalten.</p>
-    </div>
+      {/* Mobile */}
+      <div className="grid gap-5 border-t-1 border-neutral-300 py-5 text-neutral-400 md:hidden">
+        <div className="flex justify-center gap-10">
+          <Link href="/impressum">Impressum</Link>
+          <Link href="/datenschutz">Datenschutz</Link>
+        </div>
+        <p className="text-center">Copyright © 2026 andrin.software. Alle Rechte vorbehalten.</p>
+      </div>
+    </>
   )
 }
 
 export default function Footer() {
   return (
-    <footer className="section-border-t-dashed-mobile">
-      <ContentPadding>
-        <div className="py-20">
-          <div className="h-fit w-fit">
-            <Link href={"/"}>
-              <SVGLogoSmall className="w-[70px]" />
-            </Link>
+    <footer className="sm:mx-body-desktop flex-col md:flex md:items-center">
+      <div className="w-full sm:max-w-7xl">
+        <ContentPadding className="section-border-x-dashed-desktop">
+          <div className="py-20 md:flex md:items-center md:justify-between md:py-10">
+            <div className="h-fit w-fit">
+              <Link href={"/"}>
+                <SVGLogoSmall className="w-[70px]" />
+              </Link>
+            </div>
+            <FooterNavigation />
           </div>
-          <FooterNavigation />
-        </div>
-        <FooterLegal />
-      </ContentPadding>
+        </ContentPadding>
+        <ContentPadding>
+          <FooterLegal />
+        </ContentPadding>
+      </div>
     </footer>
   )
 }
